a213cb91fbcee9891f5d7f703c3872a04dbc91da
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-11-20 22:10:13 +0200

    Tehtävä 2

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -9,15 +9,17 @@ def main():
     for player_dict in response:
         player = Player(
             player_dict['name'], player_dict['nationality'], 
-            player_dict['team'], player_dict['goals'], player_dict['assists']
+            player_dict['team'], player_dict['goals'], player_dict['assists'],
+            player_dict['goals'] + player_dict['assists']
         )
 
         players.append(player)
 
     print("Players from FIN:")
+    players.sort(key=lambda p: p.points, reverse=True)
     for player in players:
         if player.nationality == "FIN":
-            print(player)
+            print(f"{player.name:21}{player.team:^4}{player.goals:^4}+{player.assists:^4}={player.points:>3}")
 
 if __name__ == "__main__":
     main()
