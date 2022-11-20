6c21b8fecf76ecd3e09cb49c586c61c41806f677
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-11-20 22:42:34 +0200

    Tehtävä 3

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,25 +1,13 @@
-import requests
-from player import Player
+from player import Player, PlayerReader, PlayerStats
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
-    response = requests.get(url).json()
+    reader = PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2021-22/players")
+    stats = PlayerStats(reader)
+    players = stats.top_scores_by_nationality("FIN")
 
-    players = []
-    for player_dict in response:
-        player = Player(
-            player_dict['name'], player_dict['nationality'], 
-            player_dict['team'], player_dict['goals'], player_dict['assists'],
-            player_dict['goals'] + player_dict['assists']
-        )
-
-        players.append(player)
-
-    print("Players from FIN:")
-    players.sort(key=lambda p: p.points, reverse=True)
+    print(f"Players from FIN:")
     for player in players:
-        if player.nationality == "FIN":
-            print(f"{player.name:21}{player.team:^4}{player.goals:^4}+{player.assists:^4}={player.points:>3}")
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,3 +1,38 @@
+import requests
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.response = requests.get(url).json()
+
+    def get_players(self):
+
+        players = []
+        for player_dict in self.response:
+            player = Player(
+                player_dict['name'], player_dict['nationality'], 
+                player_dict['team'], player_dict['goals'], player_dict['assists'],
+                player_dict['goals'] + player_dict['assists']
+            )
+
+            players.append(player)
+
+        return players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scores_by_nationality(self, nationality):
+        result = []
+        self.players.sort(key=lambda p: p.points, reverse=True)
+
+        for player in self.players:
+            if player.nationality == nationality:
+                result.append(player)
+
+        return result
+
 class Player:
     def __init__(self, name, nationality, team, goals, assists, points):
         self.name = name
@@ -8,4 +43,4 @@ class Player:
         self.points = points
     
     def __str__(self):
-        return f"{self.name} in team {self.team} has {self.goals} goals and {self.assists} assists"
+        return f"{self.name:21}{self.team:^4}{self.goals:^4}+{self.assists:^4}={self.points:>3}"
