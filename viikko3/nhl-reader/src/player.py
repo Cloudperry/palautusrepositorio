e99b91f38c221cdb72c4b08da490d3cd835ba0b2
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-11-20 21:58:57 +0200

    Tehtävä 1

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,10 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, team, goals, assists):
         self.name = name
+        self.nationality = nationality
+        self.team = team
+        self.goals = goals
+        self.assists = assists
     
     def __str__(self):
-        return self.name
+        return f"{self.name} in team {self.team} has {self.goals} goals and {self.assists} assists"
