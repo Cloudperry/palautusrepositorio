e2a641e97ebda961a713be0745dbc7dfaa8565ab
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-11-28 17:22:23 +0200

    Tehtävä 5 kohta 2

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -9,3 +9,8 @@ class TestOstoskori(unittest.TestCase):
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         assert self.kori.hinta() == 0
         assert self.kori.ostokset() == []
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        assert self.kori.tavaroita_korissa() == 1
